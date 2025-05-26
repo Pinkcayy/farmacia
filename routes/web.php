241e3320a961134e8f72adc0c3750863f56d4bbb
pinkcayy <karenc112358@gmail.com>
2025-05-25 22:52:01 -0400

    mensaje nuevo

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,16 @@ use App\Http\Controllers\ProductoController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Aquí se registran las rutas web de tu aplicación.
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Ruta raíz que muestra la lista de productos directamente
+Route::get('/', [ProductoController::class, 'index'])->name('home');
+
+// Ruta para ver todos los productos
 Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
+
+// Ruta para ver el detalle de un producto específico
 Route::get('/productos/{id}', [ProductoController::class, 'show'])->name('productos.show');
-Route::get('/productos', [ProductoController::class, 'index']);
+
